41184d92b9f46f54c113ede3ebb5850e8206c5c6
Karsten Pruehss <Karsten.Pruehss@xakiatech.com>
2022-11-19 10:39:21 +1100

    Add root route component + outlet in root.tsx

--- app/root.tsx
+++ app/root.tsx
@@ -1,16 +1,16 @@
-import { LiveReload } from "@remix-run/react";
+import { LiveReload, Outlet } from "@remix-run/react";
 
 export default function App() {
   return (
     <html lang="en">
-    <head>
-      <meta charSet="utf-8" />
-      <title>Remix: So great, it's funny!</title>
-    </head>
-    <body>
-    Hello world
-    <LiveReload />
-    </body>
+      <head>
+        <meta charSet="utf-8" />
+        <title>Remix: So great, it's funny!</title>
+      </head>
+      <body>
+        <Outlet />
+        <LiveReload />
+      </body>
     </html>
   );
 }
